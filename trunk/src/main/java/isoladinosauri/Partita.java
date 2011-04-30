e87a8f996efb0bd6fcc388cd0235804c3972f53d
bet <bet@1cf6cd4f-9c67-4b2e-ae02-52b0c5b3b7c6>
2011-04-30 17:42:02 +0000

    aggiunto POM e Db4o

--- trunk/src/main/java/isoladinosauri/Partita.java
+++ trunk/src/main/java/isoladinosauri/Partita.java
@@ -2,6 +2,10 @@ package isoladinosauri;
 
 import java.util.List;
 
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
 public class Partita {
 	
 	private Cella[][] mappa;
@@ -10,9 +14,78 @@ public class Partita {
 	private int contatoreTurni;
 	
 	
-	void daTogliere() {
-		//creo un nuovo turno
-		Turno t = new Turno(this);
-	}
+	
+	public Cella[][] getMappa() {
+        return mappa;
+    }
+
+
+
+    public void setMappa(Cella[][] mappa) {
+        this.mappa = mappa;
+    }
+
+
+
+    public Turno getTurnoCorrente() {
+        return turnoCorrente;
+    }
+
+
+
+    public void setTurnoCorrente(Turno turnoCorrente) {
+        this.turnoCorrente = turnoCorrente;
+    }
+
+
+
+    public List<Giocatore> getGiocatori() {
+        return giocatori;
+    }
+
+
+
+    public void setGiocatori(List<Giocatore> giocatori) {
+        this.giocatori = giocatori;
+    }
+
+
+
+    public int getContatoreTurni() {
+        return contatoreTurni;
+    }
+
+
+
+    public void setContatoreTurni(int contatoreTurni) {
+        this.contatoreTurni = contatoreTurni;
+    }
+
+
+
+    public static void main(String[] args) {
+	    Partita p = new Partita();
+	    Turno t = new Turno(p);
+	    t.setContatoreTurno((int)(Math.random()*100));
+	    p.setTurnoCorrente(t);
+	    
+	    //Scrive
+        ObjectContainer container = Db4o.openFile("isoladinosauri.db4o");
+        container.store(p);
+        container.commit();
+        container.close();
+	    
+        //legge
+	    ObjectContainer container2 = Db4o.openFile("isoladinosauri.db4o");
+	    ObjectSet<Partita> partite = container2.query(Partita.class);
+	    for(Partita partita : partite) {
+	        System.out.println(partita.getTurnoCorrente().getContatoreTurno());
+	    }
+	    
+	    
+        
+    }
+	
+	
 
 }
