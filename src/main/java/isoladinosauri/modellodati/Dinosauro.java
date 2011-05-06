637d229f920a2d3cd9732f08df4a176f535f4ad2
Stefano Cappa <stefano.cappa@mail.polimi.it>
2011-05-06 20:11:59 +0000

    rimosso metodo astratto in dinosauro e implentato davvero

--- src/main/java/isoladinosauri/modellodati/Dinosauro.java
+++ src/main/java/isoladinosauri/modellodati/Dinosauro.java
@@ -10,6 +10,13 @@ public abstract class Dinosauro extends Organismo implements Animale {
 	
 	public abstract void deponi(); //toglie energia per deposizione
 	public abstract void muovi(int posX, int posY); //toglie energie per movimento
-	public abstract void aumentaDimensione(); //...e toglie energia per dimensione
-
+	
+	public void aumentaDimensione() {
+		if(dimensione < 5) {
+			dimensione++;
+			super.energia -= super.energiaMax / 2;
+		}
+		/* manca l'else perche il liv massimo e' 5*/
+		//creare un else che avvisa l'utente che la dim massima e' 5
+	}
 }
