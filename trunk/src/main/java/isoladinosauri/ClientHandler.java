acb64031e6ef0f4ce5e60b344a8a3db28cd337d1
726738 <726738@1cf6cd4f-9c67-4b2e-ae02-52b0c5b3b7c6>
2011-05-28 09:39:49 +0000

    sistemati @creaUtente e @login e aggiunto menu' nella classe client per non digitare ogni volta il comando per intero

--- trunk/src/main/java/isoladinosauri/ClientHandler.java
+++ trunk/src/main/java/isoladinosauri/ClientHandler.java
@@ -2,21 +2,13 @@ package isoladinosauri;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-//import java.io.FileReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-//import java.util.Locale;
-import java.util.Formatter;
 import java.util.StringTokenizer;
-import isoladinosauri.modellodati.Carogna;
-import isoladinosauri.modellodati.Dinosauro;
-import isoladinosauri.modellodati.Vegetale;
-
-import java.util.Scanner;
 
 public class ClientHandler extends Thread {
 
@@ -24,21 +16,12 @@ public class ClientHandler extends Thread {
 
 	public ClientHandler(Socket socket) {
 		this.socket = socket;
-		//		date = new Date();
-		//		dateFormat = DateFormat.getTimeInstance(DateFormat.MEDIUM, Locale.ITALY);
 	}
 
 	public void run() {
 		StringTokenizer st = null;
 		StringTokenizer parametro = null;
-		//		StringTokenizer stFile = null;
 		String token;
-
-		//		Isola i;
-		//		Partita p = new Partita(i);
-		//		Turno t = new Turno(p);
-		//		Classifica c = new Classifica(p);
-		//		p.setTurnoCorrente(t);
 
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -47,7 +30,7 @@ public class ClientHandler extends Thread {
 				String request = bufferedReader.readLine();
 				st = new StringTokenizer(request,",");
 				String comando = st.nextToken();
-				//				String par;
+				String string;
 
 				if (comando == null) {
 					System.out.println("Client closed connection.");
@@ -55,54 +38,23 @@ public class ClientHandler extends Thread {
 				} else if (comando.equals("@creaUtente")) {
 					String nickname="";
 					String password="";
-
-					if(st.hasMoreTokens()){
-						comando=st.nextToken();
-						parametro = new StringTokenizer(comando,"=");
-						if(parametro.hasMoreTokens()){
-							String string=parametro.nextToken();
-							if(string.equals("user")) {
-								nickname=parametro.nextToken();
-							}
-							else { 
-								bufferedWriter.write("errore! richiesto parametro 'user' ");
-							}
-						}
-						//						else {
-						//							bufferedWriter.write("errore! parametro mancante");
-						//						}
-						//	}
-						//					else {
-						//						bufferedWriter.write("errore! parametro mancante");
-						//					}
-
-						if(st.hasMoreTokens()){
-							comando=st.nextToken();
-							parametro = new StringTokenizer(comando,"=");
-							if(parametro.hasMoreTokens()){
-								String string=parametro.nextToken();
-								if(string.equals("pass")) {
-									password=parametro.nextToken();
-								}
-								else { 
-									bufferedWriter.write("errore! richiesto parametro 'pass' ");
-								}
-							}
-							//						else {
-							//							bufferedWriter.write("errore! parametro mancante");
-							//						}
-						}
-						//					else {
-						//						bufferedWriter.write("errore! parametro mancante");
-						//					}
-
+	
+					comando=st.nextToken(); // comando: user=U,pass=P
+					parametro = new StringTokenizer(comando,"=");
+					string=parametro.nextToken(); // string: user
+					if(string.equals("user")) {
+						nickname=parametro.nextToken();
+					}
+					comando=st.nextToken();
+					parametro = new StringTokenizer(comando,"=");
+					string=parametro.nextToken();
+					if(string.equals("pass")) {
+						password=parametro.nextToken();
 						//cerco nel file Utenti.txt se l'utente e' gia' esistente
-
 						FileReader fileReader = new FileReader("Utenti.txt");
 						BufferedReader br;
 						br = new BufferedReader(fileReader);
 						String rigaFile = br.readLine();
-
 						boolean trovato=false;
 						while(rigaFile!=null) {
 							if(rigaFile.split(" ")[0].equals(nickname)) {
@@ -118,80 +70,47 @@ public class ClientHandler extends Thread {
 							fileUtenti.write(nickname+" "+password+"\n");
 							fileUtenti.close();
 						}
-
-						//					bufferedWriter.write("nome = "+nickname+", pass = "+password);
 						Utente utente = new Utente(nickname,password);
 					}
-					else {
-						bufferedWriter.write("errore! parametri mancanti"); 
-					}
 				}
-
+			
 				else if (comando.equals("@login")) {
-
-
 					String nickname="";
 					String password="";
 
-					if(st.hasMoreTokens()){
-						comando=st.nextToken();
-						parametro = new StringTokenizer(comando,"=");
-						if(parametro.hasMoreTokens()){
-							String string=parametro.nextToken();
-							if(string.equals("user")) {
-								nickname=parametro.nextToken();
-							}
-							else { 
-								bufferedWriter.write("errore! richiesto parametro 'user' ");
-							}
-						}
-						else {
-							bufferedWriter.write("errore! parametro mancante");
-						}
-					}
-					else {
-						bufferedWriter.write("errore! parametro mancante");
+					comando=st.nextToken();
+					parametro = new StringTokenizer(comando,"=");
+					string=parametro.nextToken();
+					if(string.equals("user")) {
+						nickname=parametro.nextToken();
 					}
 
-					if(st.hasMoreTokens()){
-						comando=st.nextToken();
-						parametro = new StringTokenizer(comando,"=");
-						if(parametro.hasMoreTokens()){
-							String string=parametro.nextToken();
-							if(string.equals("pass")) {
-								password=parametro.nextToken();
+					comando=st.nextToken();
+					parametro = new StringTokenizer(comando,"=");
+					string=parametro.nextToken();
+					if(string.equals("pass")) {
+						password=parametro.nextToken();
+						//verifico se c'e' nel file Utenti.txt
+						FileReader fileReader = new FileReader("Utenti.txt");
+						BufferedReader br;
+						br = new BufferedReader(fileReader);
+						String rigaFile = br.readLine();
+	
+						boolean trovato=false;
+						while(rigaFile!=null) {
+							if(rigaFile.split(" ")[0].equals(nickname) && rigaFile.split(" ")[1].equals(password) ) {
+								trovato=true;
+								token=nickname+password;
+								bufferedWriter.write("@Ok - token = "+token);
+								break;
 							}
-							else { 
-								bufferedWriter.write("errore! richiesto parametro 'pass' ");
-							}
+							rigaFile = br.readLine();
 						}
-						else {
-							bufferedWriter.write("errore! parametro mancante");
+						if(trovato==false) {
+							bufferedWriter.write("Utente non trovato ");
 						}
+						br.close();
 					}
-					else {
-						bufferedWriter.write("errore! parametro mancante");
-					}
-
-					FileReader fileReader = new FileReader("Utenti.txt");
-					BufferedReader br;
-					br = new BufferedReader(fileReader);
-					String rigaFile = br.readLine();
-
-					boolean trovato=false;
-					while(rigaFile!=null) {
-						if(rigaFile.split(" ")[0].equals(nickname) && rigaFile.split(" ")[1].equals(password) ) {
-							trovato=true;
-							token=nickname+password;
-							bufferedWriter.write("@Ok - token = "+token);
-							break;
-						}
-						rigaFile = br.readLine();
-					}
-					if(trovato==false) {
-						bufferedWriter.write("Utente non trovato ");
-					}
-					br.close();
 
 				}
 				else
