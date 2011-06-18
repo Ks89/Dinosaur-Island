9c94c1f85dbf3ab1299c13ee71193149e9cae6df
726738 <726738@1cf6cd4f-9c67-4b2e-ae02-52b0c5b3b7c6>
2011-06-18 20:27:23 +0000

    -tolto il commento per una eccezione nella classe turno (quella di erbivoro su erbivoro) -aggiornati i test di giocatore, partita e turno -eliminate le classi carnivoroTest e erbivoroTest perche' i loro metodi vengono gia' testati in altri test

--- src/test/java/isoladinosauri/GiocatoreTest.java
+++ src/test/java/isoladinosauri/GiocatoreTest.java
@@ -28,10 +28,8 @@ public class GiocatoreTest {
 		Partita p = inizializzaPartita();
 		Turno t = new Turno(p);
 		p.setTurnoCorrente(t);
-		Giocatore g = new Giocatore(1,"pippo","c");
+		Giocatore g = new Giocatore(1,"stego","c");
 		Utente u = new Utente("nomeUtente","pass");
-		Dinosauro d = new Carnivoro("11",1,1,1);
-		g.aggiungiDinosauro(d);
 		g.setUtente(u);
 		assertEquals(0, p.getGiocatori().size());
 		g.aggiungiInPartita(p);
@@ -39,21 +37,13 @@ public class GiocatoreTest {
 	}
 
 	/**
-	 * Test method for {@link isoladinosauri.Giocatore#generaIdDinosauro()}.
-	 */
-	@Test //FIXME: controllare
-	public void testGeneraIdDinosauro() {
-		Giocatore g = new Giocatore(1,"pippo","c");
-		assertEquals("01", g.generaIdDinosauro());
-	}
-
-
-	/**
 	 * Test method for {@link isoladinosauri.Giocatore#aggiungiDinosauro(isoladinosauri.modellodati.Dinosauro)}.
 	 */
 	@Test
 	public void testAggiungiDinosauro() {
 		Giocatore g = new Giocatore(1,"stego","c");
+		Utente u = new Utente("nomeUtente","pass");
+		g.setUtente(u);
 		Dinosauro d1 = new Carnivoro("11",1,1,1);
 		Dinosauro d2 = new Carnivoro("12",1,2,1);
 		Dinosauro d3 = new Carnivoro("13",2,1,1);
@@ -71,41 +61,48 @@ public class GiocatoreTest {
 	/**
 	 * Test method for {@link isoladinosauri.Giocatore#rimuoviDinosauro(isoladinosauri.modellodati.Dinosauro)}.
 	 */
-	@Test //TODO: rivedere x l'uso di assert
+	@Test
 	public void testRimuoviDinosauro() {
 		Partita p = inizializzaPartita();
 		Turno t = new Turno(p);
 		p.setTurnoCorrente(t);
 		Giocatore g = new Giocatore(1,"stego","c");
+		Utente u = new Utente("nomeUtente","pass");
+		g.setUtente(u);
 		g.aggiungiInPartita(p);
 		Dinosauro d = g.getDinosauri().get(0);
 		g.rimuoviDinosauro(d);
+		assertEquals(0, g.getDinosauri().size());
 	}
 
 	/**
 	 * Test method for {@link isoladinosauri.Giocatore#eseguiDeposizionedeponiUovo(isoladinosauri.modellodati.Dinosauro)}.
 	 */
-	@Test //FIXME: non lo trova nella mappa
+	@Test
 	public void testEseguiDeposizionedeponiUovo() {
+		Partita p = inizializzaPartita();
+		Turno t = new Turno(p);
+		p.setTurnoCorrente(t);
 		Giocatore g = new Giocatore(1,"stego","c");
-		Dinosauro d = new Carnivoro("11",1,1,1);
+		Utente u = new Utente("nomeUtente","pass");
+		g.setUtente(u);
+		g.aggiungiInPartita(p);
+		Dinosauro d = g.getDinosauri().get(0);
 		try {
+			g.aggiungiUovo(d.getRiga(), d.getColonna(), d.getId());
+			d.setEnergia(4000);
 			g.eseguiDeposizionedeponiUovo(d);
-			fail("non ha generato eccezioni");
+			assertEquals(2500,d.getEnergia());
 		} catch (DeposizioneException e) {
-//			e.printStackTrace();
-		} catch (NullPointerException e) {
-//			e.printStackTrace();
-		}
-//		try {
-//			g.aggiungiUovo(d.getRiga(), d.getColonna(), d.getId());
-//			d.setEnergia(10);
-//			g.eseguiDeposizionedeponiUovo(d);
-//			fail("non ha generato eccezioni");
-//		} catch (DeposizioneException e) {
-////			e.printStackTrace();
-//		} catch (NullPointerException e) {
-////			e.printStackTrace();
-//		}
+			fail("eccezione");
+		}		
+		try {
+			g.aggiungiUovo(d.getRiga(), d.getColonna(), d.getId());
+			d.setEnergia(10);
+			g.eseguiDeposizionedeponiUovo(d);
+			fail("non ha generato eccezione x mancanza di energia");
+		} catch (DeposizioneException e) {
+			System.out.println("ok generata eccezione x mancanza di energia");
+		} 
 	}
 }
