f230743da03a18ca3bdbe9e17c4724bfab22c5f5
Stefano Cappa <stefano.cappa@mail.polimi.it>
2011-06-20 10:00:02 +0000

    commit package2

--- src/main/java/client/locale/MenuGuiLocale.java
+++ src/main/java/client/locale/MenuGuiLocale.java
@@ -1,72 +1,72 @@
-package client.locale;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-
-/**
- * 	Classe che si occupa di creare il Menu del programma all'interno del JFrame.
- */
-public class MenuGuiLocale {
-
-	private JFrame frame;
-
-	/**
-	 * Costruttore della classe MenuGui che riceve il JFrame contiene tutti gli elementi dell'interfaccia.
-	 * @param frame JFrame che contiene la grafica dell'applizazione.
-	 */
-	public MenuGuiLocale(JFrame frame) {
-		this.frame = frame;
-	}
-	
-	/**
-	 * Metodo per generare il Menu
-	 * @return Una JMenuBar contenente tutti gli elementi del menu,.
-	 */
-	public JMenuBar getMenu() {
-		JMenu fileMenu = new JMenu("File"); 
-		fileMenu.setMnemonic('F'); //imposta il mnemonic ad F
-
-		//crea la voce About
-		Icon aboutIcona = new ImageIcon(getClass().getResource("/info_icon.png"));
-		JMenuItem aboutItem = new JMenuItem("Info...",aboutIcona);
-		aboutItem.setMnemonic('I'); //imposta mnemonic
-		fileMenu.add(aboutItem); //aggiunge la voce al menu file
-		aboutItem.addActionListener(
-				new ActionListener() //classe inner anonima
-				{
-					public void actionPerformed(ActionEvent event)
-					{//mostra un messaggio quando l'untente sceglie about...
-						JOptionPane.showMessageDialog(null,"Isola Dei Dinosauri BETA1\nCreato da Stefano Cappa e Luca Besana\n(C) 2011","About",JOptionPane.PLAIN_MESSAGE);
-					}
-				}
-		);
-		//crea la voce exit
-		Icon uscitaIcona = new ImageIcon(this.getClass().getResource("/Exit_icon.jpg"));
-		JMenuItem exitItem = new JMenuItem("Exit",uscitaIcona);
-		exitItem.setMnemonic('E');
-		fileMenu.add(exitItem);
-		exitItem.addActionListener(
-				new ActionListener()
-				{
-					public void actionPerformed(ActionEvent event)
-					{
-						frame.dispose();
-					}
-				}
-		);
-
-		//aggiunge il menu File alla barra appena creata
-		JMenuBar bar = new JMenuBar();
-		bar.add(fileMenu);
-		return bar;
-	}
-	
-}
+//package client.locale;
+//
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//import javax.swing.Icon;
+//import javax.swing.ImageIcon;
+//import javax.swing.JFrame;
+//import javax.swing.JMenu;
+//import javax.swing.JMenuBar;
+//import javax.swing.JMenuItem;
+//import javax.swing.JOptionPane;
+//
+///**
+// * 	Classe che si occupa di creare il Menu del programma all'interno del JFrame.
+// */
+//public class MenuGuiLocale {
+//
+//	private JFrame frame;
+//
+//	/**
+//	 * Costruttore della classe MenuGui che riceve il JFrame contiene tutti gli elementi dell'interfaccia.
+//	 * @param frame JFrame che contiene la grafica dell'applizazione.
+//	 */
+//	public MenuGuiLocale(JFrame frame) {
+//		this.frame = frame;
+//	}
+//	
+//	/**
+//	 * Metodo per generare il Menu
+//	 * @return Una JMenuBar contenente tutti gli elementi del menu,.
+//	 */
+//	public JMenuBar getMenu() {
+//		JMenu fileMenu = new JMenu("File"); 
+//		fileMenu.setMnemonic('F'); //imposta il mnemonic ad F
+//
+//		//crea la voce About
+//		Icon aboutIcona = new ImageIcon(getClass().getResource("/info_icon.png"));
+//		JMenuItem aboutItem = new JMenuItem("Info...",aboutIcona);
+//		aboutItem.setMnemonic('I'); //imposta mnemonic
+//		fileMenu.add(aboutItem); //aggiunge la voce al menu file
+//		aboutItem.addActionListener(
+//				new ActionListener() //classe inner anonima
+//				{
+//					public void actionPerformed(ActionEvent event)
+//					{//mostra un messaggio quando l'untente sceglie about...
+//						JOptionPane.showMessageDialog(null,"Isola Dei Dinosauri BETA1\nCreato da Stefano Cappa e Luca Besana\n(C) 2011","About",JOptionPane.PLAIN_MESSAGE);
+//					}
+//				}
+//		);
+//		//crea la voce exit
+//		Icon uscitaIcona = new ImageIcon(this.getClass().getResource("/Exit_icon.jpg"));
+//		JMenuItem exitItem = new JMenuItem("Exit",uscitaIcona);
+//		exitItem.setMnemonic('E');
+//		fileMenu.add(exitItem);
+//		exitItem.addActionListener(
+//				new ActionListener()
+//				{
+//					public void actionPerformed(ActionEvent event)
+//					{
+//						frame.dispose();
+//					}
+//				}
+//		);
+//
+//		//aggiunge il menu File alla barra appena creata
+//		JMenuBar bar = new JMenuBar();
+//		bar.add(fileMenu);
+//		return bar;
+//	}
+//	
+//}
